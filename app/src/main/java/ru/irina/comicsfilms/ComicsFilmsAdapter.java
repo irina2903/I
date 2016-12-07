6e91cfc23c1658415b30180228d8137bad01319d
Vladislav <farmatholin@gmail.com>
2016-12-07 18:53:06 +0300

    добавила текстовое поле, вьюшку и тд

---
+++ app/src/main/java/ru/irina/comicsfilms/ComicsFilmsAdapter.java
@@ -0,0 +1,45 @@
+package ru.irina.comicsfilms;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by User on 12/7/2016.
+ */
+
+public class ComicsFilmsAdapter extends ArrayAdapter{
+
+    public ComicsFilmsAdapter(Context context, int resource, Object[] objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+        // создаём элемент для машинки с фотографией и двумя полями
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.comicsfilms, parent, false);
+        ComicsFilms comicsFilms = (ComicsFilms) getItem(position);
+
+
+
+        TextView textView = (TextView) view.findViewById(R.id.textView);
+        textView.setText(comicsFilms.title);
+
+        ImageView imageView = (ImageView) view.findViewById(R.id.imageView);
+        Picasso.with(getContext()).load("https://upload.wikimedia.org/wikipedia/ru/5/5f/Spiderman_movie.jpg").fit().into(imageView);
+
+        return view;
+
+
+
+    }
+
+}
+
--- app/src/main/java/ru/irina/comicsfilms/MainActivity.java
+++ app/src/main/java/ru/irina/comicsfilms/MainActivity.java
@@ -1,31 +1,53 @@
 package ru.irina.comicsfilms;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class MainActivity extends AppCompatActivity {
     ComicsFilms[] фильмы;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        фильмы = new ComicsFilms[3];
-        фильмы[0] = new ComicsFilms("Человек-паук", "https://upload.wikimedia.org/wikipedia/ru/5/5f/Spiderman_movie.jpg", "Columbia Pictures", 2002, 139000000, 821708551, false );
-        фильмы[1] = new ComicsFilms("Доктор Стрэндж", "https://upload.wikimedia.org/wikipedia/ru/c/c7/Doctor_Strange_poster.jpg", "Marvel Studios", 2016, 165000000 ,620069731 , true );
-        фильмы[2] = new ComicsFilms("Дэдпул","https://upload.wikimedia.org/wikipedia/ru/b/b2/Deadpool_film.jpg", "20th Century Fox", 2016, 58000000, 782612155, true );
+        фильмы = new ComicsFilms[13];
+        фильмы[0] = new ComicsFilms("Человек-паук", "https://upload.wikimedia.org/wikipedia/ru/5/5f/Spiderman_movie.jpg", "Columbia Pictures", 2002, 139000000, 821708551, false);
+        фильмы[1] = new ComicsFilms("Доктор Стрэндж", "https://upload.wikimedia.org/wikipedia/ru/c/c7/Doctor_Strange_poster.jpg", "Marvel Studios", 2016, 165000000, 620069731, true);
+        фильмы[2] = new ComicsFilms("Дэдпул", "https://upload.wikimedia.org/wikipedia/ru/b/b2/Deadpool_film.jpg", "20th Century Fox", 2016, 58000000, 782612155, true);
+        фильмы[3] = new ComicsFilms("Первый мститель: Противостояние", "https://upload.wikimedia.org/wikipedia/ru/thumb/b/b5/..", "Marvel Studios", 2016, 250000000, 1153304495, true);
+        фильмы[4] = new ComicsFilms("Тор", "https://upload.wikimedia.org/wikipedia/ru/f/fc/Thor_p..", "Marvel Studios", 2011, 150000000, 449326618, false);
+        фильмы[5] = new ComicsFilms("Тор 2: Царство тьмы", "https://upload.wikimedia.org/wikipedia/ru/0/04/Thor_T..", "Marvel Studios", 2013, 170000000, 644571402, false);
+        фильмы[6] = new ComicsFilms("Мстители", "https://upload.wikimedia.org/wikipedia/ru/3/3c/The_Av..", "Marvel Studios", 2012, 220000000, 1518812988, false);
+        фильмы[7] = new ComicsFilms("Мстители: Эра Альтрона", "https://upload.wikimedia.org/wikipedia/ru/0/04/The_Av..", "Marvel Studios", 2015, 250000000, 1405403694, false);
+        фильмы[8] = new ComicsFilms("Бэтмен против Супермена: На заре справедливости", "https://upload.wikimedia.org/wikipedia/ru/thumb/f/f0/..",
+                "DC Entertainment", 2016, 250000000, 873260194, true);
+        фильмы[9] = new ComicsFilms("Отряд самоубийц", "https://upload.wikimedia.org/wikipedia/ru/4/4d/Suicid..", "Warner Bros", 2016, 175000000, 745600054, true);
+        фильмы[10] = new ComicsFilms("Железный человек", "https://upload.wikimedia.org/wikipedia/ru/3/30/Iron_man_filmposter.jpg", "Marvel Studios", 2008, 140000000, 585174222, false);
+        фильмы[11] = new ComicsFilms("Первый мститель", "https://upload.wikimedia.org/wikipedia/ru/4/44/CaptainAmerica.jpg", "Marvel Studios", 2011, 140000000, 370569774, false);
+        фильмы[12] = new ComicsFilms("Человек-муравей", "https://upload.wikimedia.org/wikipedia/ru/7/75/Ant-Man_poster.jpg", "Marvel Studios", 2015, 130000000, 519311965, false);
+
 
         ListView listView = (ListView) findViewById(R.id.ListView);
         int itemLayout = android.R.layout.simple_list_item_1;
-        ArrayAdapter adapter = new ArrayAdapter<ComicsFilms>(this, itemLayout,фильмы);
+        ArrayAdapter adapter = new ComicsFilmsAdapter(this, itemLayout, фильмы);
         listView.setAdapter(adapter);
 
 
-
-
     }
+
 }
